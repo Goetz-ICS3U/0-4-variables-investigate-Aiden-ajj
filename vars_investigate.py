ab2af87ea6371560414e54547a7fe9b81b5f0fab
Aiden-ajj <354260403@educ.dpcdsb.org>
2026-02-10 12:36:51 -0500

    Change value of all variabkles and added new variables with more text in the print function.

---
+++ vars_investigate.py
@@ -0,0 +1,43 @@
+"""
+author: Aiden Amparo
+date:Febuarary 10 2026
+Investigating Variables
+"""
+
+# Input
+name = "Aiden Amparo"
+grade = 11
+favourite_exclamation = "Yeo"
+least_favourite_colour = "blue"
+is_cool = True
+math_test_score = 90
+sister_name = None
+
+slope = 2
+y_intercept = 10
+x = 3
+y = slope * x + y_intercept
+
+favourite_food = "Buldak Carbonara"
+moms_name = "Chrismelyn"
+friends_name = "Daquavis Jones"
+
+
+
+
+# Processing / Output
+print(
+    f"Hey guys, my name is {name} and I am currently in grade {grade}. "
+    + f"Yesterday, there was a spider in the classroom that made me yell {favourite_exclamation} whiile I was taking a math test with my {least_favourite_colour} pen :/ "
+    + f"One of the questions on the test was wether or not I thought Elvis was cool, so I obviously put {is_cool} to get the answer right... "
+    + f"Turns out that I only scored a {math_test_score} on the test. One of the qusetions was asking me to write the equation for a line "
+    + f"with a slope of {slope} and a y-intercept of {y_intercept} so I wrote y = {slope}x + {y_intercept}, but I did not notice the x value of {x} in the question "
+    + f"so I needed to, on the next line, say that the value of y was {y}. "
+    + f"I told my sister {sister_name} about this and she just laughed at me 🙄"
+    + "Anyway, that's the story of my math test."
+    f"After my mom, {moms_name} picked me up from school she asked me what I wanted to eat when we get home"
+    f"I told her about how I got a {math_test_score} on the math test so I feel that I deserve my {favourite_food}!!"
+    f"When I told my mom this she was happy for me and said {favourite_exclamation} you should be proud of a {math_test_score}"
+    f"She then told me that I can go out with {friends_name} and my friends after we go home, since I deserve it."
+    "So that is how my day has been going today."
+)
